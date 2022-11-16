962bb3100cc569d6b47d3fd2404d768c5f273729
leesijun <season2lee@gmail.com>
2022-11-16 14:05:05 +0900

    1116 first

---
+++ final-pjt-back/articles/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.conf import settings
+from movies.models import Movie
+
+# Create your models here.
+class Article(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+# class Image(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to='diary', blank=True, null=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ final-pjt-back/movies/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=250)
+    release_date = models.TextField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    poster_path = models.TextField()
+    adult = models.BooleanField()
+    genres = models.ManyToManyField(Genre)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.TextField(max_length=500)
